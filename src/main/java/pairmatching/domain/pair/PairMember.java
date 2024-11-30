c852f3e07142cfca9c63b2aca69d728401c11bb7
sotogito <108535526+sotogito@users.noreply.github.com>
2024-11-30 18:46:07 +0900

    feat : 동일한 페어 매칭 equals 정의

---
+++ src/main/java/pairmatching/domain/pair/PairMember.java
@@ -0,0 +1,40 @@
+package pairmatching.domain.pair;
+
+import java.util.List;
+import java.util.Objects;
+import pairmatching.domain.Crew;
+
+public class PairMember {
+    private final List<Crew> crews;
+
+    public PairMember(List<Crew> crews) {
+        validateSize(crews);
+        this.crews = crews;
+    }
+
+    public void validateSize(List<Crew> crews){
+        if(crews.size() != 2 && crews.size() != 3){
+            throw new IllegalArgumentException("패어 매칭 인원 오류");
+        }
+    }
+
+    //todo 동일한 리스트인지 확인
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PairMember pairMember = (PairMember) o;
+        int count = 0;
+        for(Crew crew : crews) {
+            if(pairMember.crews.contains(crew)) {
+                count ++;
+            }
+        }
+        return count == crews.size();
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(crews);
+    }
+
+}
