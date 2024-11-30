8237b33c2d22e128c7f13187db7cda4124f92838
JinGoo <zzgg3040@naver.com>
2024-11-30 18:02:25 +0900

    test: 크루 정보 파싱

---
+++ src/test/java/pairmatching/BackEndCrewReaderTest.java
@@ -0,0 +1,37 @@
+package pairmatching;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pairmatching.constant.Course;
+
+class BackEndCrewReaderTest {
+
+    @DisplayName("파일을 읽어 리스트에 크루 정보를 저장한다.")
+    @Test
+    void test1() {
+        // given
+        BackEndCrewReader reader = new BackEndCrewReader();
+        // when
+        List<Crew> crews = reader.read();
+        // then
+        assertThat(crews)
+                .size().isEqualTo(20);
+    }
+
+    @DisplayName("각 크루의 정보는 객체에 파싱되어 저장된다.")
+    @Test
+    void test2() {
+        // given
+        BackEndCrewReader reader = new BackEndCrewReader();
+        // when
+        List<Crew> crews = reader.read();
+        // then
+        assertThat(crews.get(0))
+                .hasFieldOrPropertyWithValue("name", "백호")
+                .hasFieldOrPropertyWithValue("course", Course.BACKEND);
+    }
+}
