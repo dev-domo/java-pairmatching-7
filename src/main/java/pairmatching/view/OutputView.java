53f360ffc54dd66289fefa94e5aedbaa065abd70
JinGoo <zzgg3040@naver.com>
2024-11-30 18:57:28 +0900

    feat: 패어매칭 서비스 선택 사항 출력

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,6 +1,8 @@
 package pairmatching.view;
 
+import pairmatching.constant.Course;
 import pairmatching.constant.Menu;
+import pairmatching.constant.MissionMapper;
 
 public class OutputView {
 
@@ -9,5 +11,12 @@ public class OutputView {
 
     public static void printMenus() {
         System.out.println(Menu.getMenus());
+    }
+
+    public static void printMatchingSelections() {
+        System.out.println("#############################################");
+        System.out.println("과정: " + Course.getCourses());
+        System.out.println("미션:\n" + MissionMapper.getMissions());
+        System.out.println("#############################################");
     }
 }
